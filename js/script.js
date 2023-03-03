523576bbbe872cf76c88be1d83bc5635d21c882e
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-03 15:00:15 +0100

    feat: finish main exercise without bonus

--- js/script.js
+++ js/script.js
@@ -15,15 +15,63 @@
 
 /*
 
-- seleziono il bottone dal DOM e lo memorizzo in una variabile
-- seleziono il contenutore della mia griglia dal DOM e lo memorizzo in una variabile
+x seleziono il bottone dal DOM e lo memorizzo in una variabile
+x seleziono il contenutore della mia griglia dal DOM e lo memorizzo in una variabile
 - AL CLICK del bottone: 
-    - Creo 100 <div> 
-    - Assegno una classe .square ad ogni elemento creato
-    - Appendo ogni <div> al contenitore letto dal DOM
-    - Assegno ad ogni <div> un numero progressivo da 1 a 100
+    x Creo 100 <div>
+    x Assegno una classe .square ad ogni elemento creato
+    x Assegno ad ogni <div> un numero progressivo da 1 a 100
+    x Appendo ogni <div> al contenitore letto dal DOM
+    
     - AL CLICK della cella: 
-        - assegno all'elemento una classe che la colorerà di azzurro 
-        - stampo in console il numero della cella cliccata
+        x assegno all'elemento una classe che la colorerà di azzurro 
+        x stampo in console il numero della cella cliccata
 
 */
+
+
+// CODE
+
+
+// - seleziono il bottone dal DOM e lo memorizzo in una variabile
+const btnGenerateEl = document.getElementById('generate-grid');
+
+// - seleziono il contenutore della mia griglia dal DOM e lo memorizzo in una variabile
+const gridContainerEl = document.getElementById('grid-container');
+
+// - AL CLICK del bottone: 
+
+btnGenerateEl.addEventListener('click', function(){
+    // inizializzo un contatore per il mio ciclo
+    let i = 0;
+
+    while(i<100){
+        // memorizzo una variabile contatore
+        let count = i;
+
+        // - Creo 100 <div> 
+        let newDivEl = document.createElement('div');
+
+        // - Assegno una classe .square ad ogni elemento creato
+        newDivEl.classList.add('square');
+        
+        // - Assegno ad ogni <div> un numero progressivo da 1 a 100
+        newDivEl.innerText = i+1;
+        
+        // - AL CLICK della cella:
+        newDivEl.addEventListener('click', function(){
+            // - assegno all'elemento una classe che la colorerà di azzurro 
+            newDivEl.classList.toggle('light-blue');
+
+            // - stampo in console il numero della cella cliccata
+            console.log(`Hai cliccato la cella n. ${count+1}`);
+        });
+
+
+        // - Appendo ogni <div> al contenitore letto dal DOM
+        gridContainerEl.append(newDivEl);
+
+        // aggiorno il contatore per uscire dal loop
+        i++;
+    }
+});
